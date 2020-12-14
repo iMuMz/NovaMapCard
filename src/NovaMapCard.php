7ea0a762fc119f5663e69240afcd2cf5bd0391e9
Chad <imumz@icloud.com>
2020-12-14 09:09:22 +0200

    Updates

--- src/NovaMapCard.php
+++ src/NovaMapCard.php
@@ -22,4 +22,73 @@ class NovaMapCard extends Card
     {
         return 'nova-map-card';
     }
+
+    public function height($height){
+        return $this->withMeta([
+            'height' => $height
+        ]);
+    }
+    public function zoom($zoom){
+        return $this->withMeta([
+            'zoom' => $zoom
+        ]);
+    }
+    public function geoJson($geoJson){
+
+        return $this->withMeta([
+            'geoJson' => $geoJson
+        ]);
+    }
+    public function mapIconUrl($mapIconUrl,$mapIconSize = null,$mapIconAnchor = null){
+
+        return $this->withMeta([
+            'mapIconUrl' => $mapIconUrl,
+            'mapIconSize' => $mapIconSize,
+            'mapIconAnchor' => $mapIconAnchor
+        ]);
+    }
+
+    public function type($type){
+        return $this->withMeta([
+            'type' => $type
+        ]);
+    }
+    public function popupName($popupName){
+        return $this->withMeta([
+            'popupName' => $popupName
+        ]);
+    }
+    public function point($latitude,$longitude) {
+        return $this->withMeta([
+            'latitude' => $latitude,
+            'longitude' => $longitude
+        ]);
+    }
+
+    public function center($centerLat,$centerLon) {
+        return $this->withMeta([
+            'centerLat' => $centerLat,
+            'centerLon' => $centerLon
+        ]);
+    }
+
+    /*public function html($html) {
+        return $this->withMeta([
+            'html' => $html,
+        ]);
+    }*/
+
+    public function googleApiKey($googleApiKey) {
+
+        return $this->withMeta([
+            'googleApiKey' => $googleApiKey
+        ]);
+    }
+
+    public function googleMapType($googleMapType) {
+
+        return $this->withMeta([
+            'googleMapType' => $googleMapType
+        ]);
+    }
 }
