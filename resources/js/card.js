28f4ef28f923f9e634963b0750b3b9c3f27e5603
Chad <imumz@icloud.com>
2020-12-14 07:03:41 +0200

    Initial commit

---
+++ resources/js/card.js
@@ -0,0 +1,3 @@
+Nova.booting((Vue, router, store) => {
+  Vue.component('nova-map-card', require('./components/Card'))
+})
---
+++ src/NovaMapCard.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Imumz\NovaMapCard;
+
+use Laravel\Nova\Card;
+
+class NovaMapCard extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = '1/3';
+
+    /**
+     * Get the component name for the element.
+     *
+     * @return string
+     */
+    public function component()
+    {
+        return 'nova-map-card';
+    }
+}
